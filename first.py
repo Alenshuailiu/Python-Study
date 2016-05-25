6c046c92f87e9f11edaa0b2523252b8edde14eca
Alenshuailiu <ls851118@163.com>
2016-05-25 16:20:31 +0800

    Create first.py

---
+++ first.py
@@ -0,0 +1,9 @@
+num=input('Enter a number ')
+print('The number you entered is ',num)
+num=int(num)
+if num > 10:
+	print('you enter a number > 10')
+elif num > 5:
+	print('you enter a number >5 <10')
+else:
+	print('Others')
